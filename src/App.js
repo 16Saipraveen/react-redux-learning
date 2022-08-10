1189a2d21759a96c45c2da420ecf9f65ad602a94
Praveen_Choragudi <Praveen_Choragudi@epam.com>
2022-08-10 12:14:14 +0530

    react-redux-learning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { Home } from "./views/Home";
+import { Cart } from "./views/Cart";
+import {NavBar} from './components/NavBar'
+import store from "./store/store";
+
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +25,14 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
+    </Provider>
   );
 }
 
---
+++ src/store/productSlice.js
@@ -0,0 +1,62 @@
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
+
+export const STATUSES = Object.freeze({
+  IDLE: "idle",
+  ERROR: "error",
+  LOADING: "loading",
+});
+
+const productSlice = createSlice({
+  name: "product",
+  initialState: {
+    data: [],
+    status: STATUSES.IDLE,
+  },
+  reducers: {
+    // setProducts(state, action) {
+    //   state.data = action.payload;
+    // },
+    // setStatus(state, action) {
+    //   state.status = action.payload;
+    // },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state, action) => {
+        state.status = STATUSES.LOADING;
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.status = STATUSES.IDLE;
+        state.data = action.payload;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = STATUSES.ERROR;
+      });
+  },
+});
+
+export const { setProducts, setStatus } = productSlice.actions;
+
+export default productSlice.reducer;
+
+// Thunks
+// export function fetchProducts() {
+//   return async function fetchProductThunk(dispath, getState) {
+//     dispath(setStatus(STATUSES.LOADING));
+//     try {
+//       const res = await fetch("https://fakestoreapi.com/products");
+//       const data = await res.json();
+//       dispath(setProducts(data));
+//       dispath(setStatus(STATUSES.IDLE));
+//     } catch (error) {
+//       console.log(error);
+//       dispath(setStatus(STATUSES.ERROR));
+//     }
+//   };
+// }
+
+export const fetchProducts = createAsyncThunk("products/fetch", async () => {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data = await res.json();
+  return data;
+});
